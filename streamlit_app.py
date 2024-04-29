e818ff3d6792a60be58ea71919f4f6c8f2f001b2
Eduardo Amarante <45640708+emsamarante@users.noreply.github.com>
2024-04-29 13:21:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ with col1:
         x_img = np.linspace(df1['Y'].min(), df1['Y'].max(), img.shape[1])
         z_img = np.linspace(min(z), max(z), img.shape[0])
         X_img, Z_img = np.meshgrid(x_img, z_img)
-        ax.plot_surface(X_img, np.full_like(X_img, y_value), Z_img, facecolors=img, shade=False, alpha=alpha_1)
+        # ax.plot_surface(X_img, np.full_like(X_img, y_value), Z_img, facecolors=img, shade=False, alpha=alpha_1)
         ax.text(0.5, y_value, 1.1, "CE-01", color='red', fontsize=12, ha='center')
     #
     #
